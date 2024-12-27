2f40f6567597697f1940cac407f9f86d29e41483
luucongdac <50321469+luucongdac@users.noreply.github.com>
2024-12-27 09:59:09 +0700

    Add files via upload

---
+++ thetake.js
@@ -0,0 +1,290 @@
+const rawData = `
+- c#o#n#g#d#a#c#0#9#@g#m#a#i#l#.c#o#m#
+- --------------------[s#h#a#r#e#d# i#n#d#i#c#a#t#o#r# p#r#i#v#a#t#e#d#]------------------
+- 【T#h#e#☯T#a#k#e#】:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/s#c#r#i#p#t#/e#I#d#B#8#h#n#Q#-T#h#e#-T#a#k#e#/
+- ☯: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/s#c#r#i#p#t#/k#0#4#P#s#y#v#B#/
+- ☯ a#l#e#r#t#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/s#c#r#i#p#t#/L#6#a#5#X#b#y#Z#-a#l#e#r#t#/
+- A#l#e#r#t# V#o#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/s#c#r#i#p#t#/8#h#u#O#U#p#b#V#-A#l#e#r#t#-m#u#l#t#i#-s#y#m#b#o#l#s#-a#t#-s#p#e#c#i#f#i#c#-t#i#m#e#f#r#a#m#e#-i#n#-N#-d#a#y#-l#o#o#k#-b#a#c#k#/
+- ------------------------------------------------------------------
+- --------------------------[l#i#s#t# s#y#m#b#o#l#]----------------------------
+- l#i#s#t# s#y#m#b#o#l#s#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/w#a#t#c#h#l#i#s#t#s#/6#3#9#1#0#8#6#4#/
+- d#e#f#a#u#l#t#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/
+- 
+- ---------------------------[1#m#]-------------------------------------
+- F#o#r#e#x#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/J#e#d#v#V#j#K#g#/
+- E#u#r#o#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/3#n#o#H#z#a#Y#d#/
+- A#s#i#a#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/j#h#S#v#8#a#l#A#/
+- S#t#o#c#k#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/6#c#R#y#X#n#S#V#/
+- S#t#o#c#k#-U#S#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/c#C#8#6#m#B#D#K#/
+- C#o#i#n#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/w#Y#N#Y#z#2#M#P#/
+- U#S#D#_#E#U#R#_#J#P#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/u#1#3#3#s#w#4#1#/
+-
+- ---------------------------[5#m#]-------------------------------------
+- F#o#r#e#x#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/c#G#I#T#l#U#j#w#/
+- E#u#r#o#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/t#O#M#J#2#k#r#l#/
+- A#s#i#a#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/h#C#2#y#F#X#h#M#/
+- M#e#t#a#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/u#T#E#c#k#J#e#x#/ 
+- S#t#o#c#k#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/b#a#b#H#Y#H#B#R#/
+- C#o#i#n#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/H#O#z#Q#e#h#n#f#/
+- U#S#D#_#E#U#R#_#J#P#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/D#l#Z#r#a#E#j#o#/
+- 
+- ---------------------------[1#5#m#]-------------------------------------
+- F#o#r#e#x#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/O#1#o#Z#X#V#8#m#/
+- E#u#r#o#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/H#L#D#m#6#c#A#z#/
+- A#s#i#a#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/v#x#M#D#n#j#6#k#/
+- M#e#t#a#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/Q#6#c#0#1#k#W#D#/
+- S#o#f#t#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/6#t#u#W#K#8#z#z#/
+- S#t#o#c#k#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/j#Q#K#6#f#k#l#D#/
+- S#t#o#c#k#-U#S#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/U#j#c#R#k#L#K#W#/
+- C#o#i#n#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/y#9#E#0#1#I#7#B#/
+- U#S#D#_#E#U#R#_#J#P#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/1#N#n#e#O#z#m#O#/
+-
+- ---------------------------[1#H#]-------------------------------------
+- F#o#r#e#x#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/k#1#V#f#m#K#B#P#/
+- E#u#r#o#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/X#Y#X#k#B#4#4#r#/
+- A#s#i#a#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/o#y#M#3#7#J#X#H#/
+- M#e#t#a#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/T#3#f#m#e#W#1#9#/
+- S#o#f#t#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/c#E#5#w#z#f#f#R#/
+- S#t#o#c#k#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/I#W#X#8#z#3#V#k#/
+- S#t#o#c#k#-U#S#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/D#L#n#G#w#d#0#C#/
+- S#t#o#c#k#_#V#N#1#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/d#D#i#G#5#z#N#U#/
+- S#t#o#c#k#_#V#N#2#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/6#Q#G#N#2#p#d#s#/
+- C#o#i#n#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/2#Q#2#G#S#R#g#w#/
+- U#S#D#_#E#U#R#_#J#P#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/N#v#T#h#V#U#6#1#/
+-
+- ---------------------------[2#H#]-------------------------------------
+- F#o#r#e#x#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/4#N#y#0#I#g#B#g#/
+- E#u#r#o#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/Y#7#P#Q#j#7#e#Z#/
+- A#s#i#a#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/b#a#X#z#O#Y#N#B#/
+- M#e#t#a#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/E#g#C#D#r#Q#7#P#/
+- S#o#f#t#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/C#7#u#3#i#R#8#u#/
+- S#t#o#c#k#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/V#L#k#e#T#s#h#h#/
+- S#t#o#c#k#-U#S#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/f#m#Z#8#q#o#t#M#/
+- S#t#o#c#k#_#V#N#1#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/k#C#y#F#0#S#H#8#/
+- S#t#o#c#k#_#V#N#2#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/r#L#0#A#0#2#s#X#/
+- C#o#i#n#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/K#i#o#s#S#B#P#8#/
+- U#S#D#_#E#U#R#_#J#P#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/w#N#x#E#y#I#y#B#/
+-
+- ---------------------------[4#H#]-------------------------------------
+- F#o#r#e#x#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/4#B#s#t#s#7#5#O#/
+- E#u#r#o#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/X#U#X#j#n#i#m#7#/
+- A#s#i#a#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/t#d#B#1#A#Z#N#b#/
+- M#e#t#a#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/b#J#z#W#n#9#a#C#/
+- S#o#f#t#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/f#N#Z#5#R#e#b#9#/
+- S#t#o#c#k#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/k#A#2#6#T#k#J#Y#/
+- S#t#o#c#k#-U#S#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/L#m#d#4#K#f#7#w#/
+- S#t#o#c#k#_#V#N#1#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/k#A#P#t#Z#G#5#E#/
+- S#t#o#c#k#_#V#N#2#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/Y#8#j#m#f#c#r#d#/
+- C#o#i#n#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/W#R#i#P#b#J#F#L#/
+- U#S#D#_#E#U#R#_#J#P#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/G#n#z#C#p#l#L#E#/
+-
+- ---------------------------[1#D#]-------------------------------------
+- F#o#r#e#x#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/L#6#h#V#f#u#m#d#/
+- E#u#r#o#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/i#U#8#B#S#Q#T#Y#/
+- A#s#i#a#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/h#t#y#C#3#u#B#y#/
+- M#e#t#a#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/A#r#b#1#3#g#Y#e#/
+- S#o#f#t#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/Z#J#1#0#C#R#Z#L#/
+- S#t#o#c#k#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/S#B#C#S#E#D#i#P#/
+- S#t#o#c#k#-U#S#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/w#k#H#K#p#t#O#l#/
+- S#t#o#c#k#_#V#N#1#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/e#9#v#p#V#m#d#2#/
+- S#t#o#c#k#_#V#N#2#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/s#G#B#C#6#C#l#y#/
+- C#o#i#n#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/z#n#l#F#N#4#y#p#/
+- U#S#D#_#E#U#R#_#J#P#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/m#U#1#T#G#T#7#A#/
+-
+- ---------------------------[1#W#]-------------------------------------
+- F#o#r#e#x#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/a#p#S#1#i#8#N#f#/
+- E#u#r#o#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/5#k#x#0#w#f#p#N#/
+- A#s#i#a#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/R#s#P#s#l#Y#K#6#/
+- M#e#t#a#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/C#o#f#7#1#c#Z#k#/
+- S#o#f#t#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/z#Y#Q#l#r#q#d#V#/
+- S#t#o#c#k#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/b#1#n#e#W#J#N#S#/
+- S#t#o#c#k#-U#S#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/v#m#H#J#B#e#1#H#/
+- S#t#o#c#k#_#V#N#1#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/S#X#k#p#e#d#q#g#/
+- S#t#o#c#k#_#V#N#2#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/r#r#5#V#W#C#b#L#/
+- C#o#i#n#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/5#7#F#t#X#l#A#M#/
+- U#S#D#_#E#U#R#_#J#P#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/l#J#t#0#1#k#8#7#/
+-
+- ---------------------------[1#M#N#]-------------------------------------
+- F#o#r#e#x#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/Q#m#P#d#a#C#F#m#/
+- E#u#r#o#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/L#y#m#n#V#V#c#i#/
+- A#s#i#a#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/r#F#W#1#9#v#B#i#/
+- M#e#t#a#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/h#X#I#E#u#4#l#s#/
+- S#o#f#t#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/M#9#H#B#O#A#0#P#/
+- S#t#o#c#k#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/l#9#R#s#3#2#N#5#/
+- S#t#o#c#k#-U#S#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/h#7#M#F#U#7#A#9#/
+- S#t#o#c#k#_#V#N#1#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/D#9#j#q#2#u#f#o#/
+- S#t#o#c#k#_#V#N#2#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/B#G#6#2#p#N#r#f#/
+- C#o#i#n#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/f#v#L#Z#A#b#Z#q#/
+- U#S#D#_#E#U#R#_#J#P#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/3#h#P#p#N#Z#K#Y#/
+-
+- ---------------------------[1#Y#]-------------------------------------
+- F#o#r#e#x#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/k#t#a#I#u#f#p#t#/
+- E#u#r#o#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/h#h#t#A#V#8#G#u#/
+- A#s#i#a#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/v#L#u#N#j#S#q#E#/
+- M#e#t#a#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/i#l#O#c#s#Z#l#B#/
+- S#o#f#t#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/g#U#h#q#e#t#e#B#/
+- S#t#o#c#k#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/x#g#d#E#C#X#j#m#/
+- S#t#o#c#k#-U#S#:  h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/H#z#K#y#M#K#8#B#/
+- S#t#o#c#k#_#V#N#1#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/5#S#F#t#T#V#M#s#/
+- S#t#o#c#k#_#V#N#2#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/R#r#c#1#p#9#E#D#/
+- C#o#i#n#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/M#P#3#4#7#l#H#p#/
+- U#S#D#_#E#U#R#_#J#P#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/g#f#W#S#h#v#e#X#/
+-
+- ---------------------------[g#e#n#e#r#a#l#]-------------------------------
+- D#X#Y#_#U#S#5#0#0#_#B#T#C#_#V#N#3#0#_#1#W#-1#H#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/F#b#u#u#V#p#j#i#/
+- D#X#Y#_#U#S#5#0#0#_#B#T#C#_#V#N#3#0#_#1#H#-1#m#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/b#n#Q#K#q#3#5#7#/
+- U#S#5#0#0#_#B#T#C#_#X#A#U#_#O#I#L#_#1#W#-1#H#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/Q#V#O#N#a#R#W#O#/
+- U#S#5#0#0#_#B#T#C#_#X#A#U#_#O#I#L#_#1#H#-1#m#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/e#j#0#q#1#u#4#0#/
+- D#X#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/E#C#J#F#F#G#N#y#/
+- J#X#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/L#G#S#i#D#G#m#D#/
+- E#X#Y#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/s#y#c#5#G#V#l#Y#/
+- U#S#D#_#E#U#R#_#J#P#Y#_#1#W#-1#H#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/G#y#O#Z#g#Y#W#Z#/
+- U#S#D#_#E#U#R#_#J#P#Y#_#1#Y#-1#W#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/7#d#M#q#i#C#C#R#/
+-
+- A#U#S#2#0#0#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/g#V#1#z#p#p#L#j#/
+- H#K#5#0#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/e#z#4#i#W#5#r#l#/
+- C#H#I#N#A#5#0#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/S#V#t#d#1#J#N#I#/
+- U#K#1#0#0#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/k#O#M#a#l#Y#4#t#/
+- G#E#R#4#0#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/7#g#g#A#5#S#B#t#/
+- J#P#N#2#2#5#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/x#X#I#E#B#m#B#H#/
+- V#N#3#0#_#f#u#t#u#r#e#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/8#s#K#1#p#F#N#f#/
+- V#N#3#0#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/g#K#0#V#K#s#E#0#/
+- V#N#I#N#D#E#X#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/m#s#x#0#G#3#i#0#/
+- U#S#3#0#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/O#9#8#o#3#t#g#q#/
+- U#S#5#0#0#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/x#o#v#r#4#T#I#S#/
+- O#i#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/q#h#n#K#A#l#N#z#/
+- S#i#l#v#e#r#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/z#b#o#k#C#q#f#1#/
+- G#o#l#d#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/v#F#X#R#A#4#v#t#/
+- B#N#B#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/y#F#H#5#T#J#q#j#/
+- E#T#H#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/y#c#5#6#k#T#H#N#/
+- B#T#C#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/i#h#J#O#I#y#6#E#/
+-
+-
+- C#o#f#f#e#e#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/z#C#H#S#s#u#3#f#/
+- C#o#t#t#o#n#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/Z#m#c#b#z#1#G#U#/
+- S#u#g#a#r#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/m#H#J#B#r#P#V#e#/
+- S#o#y#b#e#a#n#s#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/l#A#Q#l#B#h#S#5#/
+- C#o#c#o#a#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/1#b#P#e#P#K#B#e#/
+- R#i#g#h#R#i#c#e#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/w#x#q#M#N#j#Z#x#/
+- C#o#r#n#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/k#g#M#X#y#A#o#V#/
+- S#o#y#M#e#a#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/y#Y#M#5#7#B#p#F#/
+- S#o#y#O#i#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/G#d#2#w#9#g#t#G#/
+-
+-
+- P#l#a#t#i#u#m#, X#P#T#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/h#S#E#6#6#E#1#U#/
+- A#l#u#, A#l#u#m#i#n#i#u#m#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/k#s#s#h#X#5#m#p#/
+- C#o#p#p#e#r#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/Q#j#l#X#A#x#0#w#/
+- N#i#c#k#e#l#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/a#Y#k#z#f#M#J#V#/
+- L#e#a#d#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/f#W#8#s#L#o#U#v#/
+- Z#i#n#c#: h#t#t#p#s#://w#w#w#.t#r#a#d#i#n#g#v#i#e#w#.c#o#m#/c#h#a#r#t#/t#t#D#h#Y#v#G#j#/
+
+`;
+const container = document.getElementById("container");
+
+// Hàm tạo màu tối
+const randomDarkColor = () => {
+    const r = Math.floor(Math.random() * 100);
+    const g = Math.floor(Math.random() * 100);
+    const b = Math.floor(Math.random() * 100);
+    return `rgb(${r}, ${g}, ${b})`;
+};
+
+// Hàm tạo màu sáng
+const randomLightColor = () => {
+    const r = Math.floor(Math.random() * 156) + 100;
+    const g = Math.floor(Math.random() * 156) + 100;
+    const b = Math.floor(Math.random() * 156) + 100;
+    return `rgb(${r}, ${g}, ${b})`;
+};
+
+// Bước 1: Làm sạch dữ liệu
+const cleanData = rawData
+    .split("\n")
+    .map(line => line.replace(/#/g, "").trim())
+    .filter(line => line.length > 0);
+
+// Bước 2: Phân tích dữ liệu
+let currentGroup = null;
+const groups = [];
+
+cleanData.forEach(line => {
+    if (line.startsWith("- ---------------------------[")) {
+        const groupName = line.match(/\[([^\]]+)\]/)?.[1]?.trim();
+        if (groupName) {
+            currentGroup = { name: groupName, items: [] };
+            groups.push(currentGroup);
+        }
+    } else if (line.startsWith("-") && currentGroup) {
+        const match = line.match(/-\s*([^:]+):\s*(https?:\/\/[^\s]+)/);
+        if (match) {
+            const name = match[1].trim();
+            const link = match[2].trim();
+            currentGroup.items.push({ name, link });
+        }
+    }
+});
+
+// Bước 3: Hiển thị các nhóm và nút
+groups.forEach(group => {
+    const groupLabel = document.createElement("h2");
+    groupLabel.textContent = group.name.toUpperCase();
+    groupLabel.style.color = randomLightColor();
+    container.appendChild(groupLabel);
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.style.display = "flex";
+    buttonContainer.style.flexWrap = "wrap";
+    buttonContainer.style.gap = "20px";
+
+    group.items.forEach((item, index) => {
+        const button = document.createElement("button");
+        button.textContent = item.name.toUpperCase();
+        button.style.backgroundColor = randomLightColor();
+        button.style.color = randomDarkColor();
+        button.style.fontWeight = "bold";
+        button.style.textTransform = "uppercase";
+        button.style.borderRadius = "15px";
+        button.style.padding = "10px 15px";
+        button.style.border = "none";
+        button.style.cursor = "pointer";
+        button.onclick = () => window.open(item.link, "_blank");
+
+        buttonContainer.appendChild(button);
+
+        if ((index + 1) % 11 === 0) {
+            const lineBreak = document.createElement("div");
+            lineBreak.style.flexBasis = "100%";
+            buttonContainer.appendChild(lineBreak);
+        }
+    });
+
+    container.appendChild(buttonContainer);
+});
+
+// Xử lý Play/Stop YouTube
+let isVideoPlaying = false;
+document.getElementById("play-button").addEventListener("click", () => {
+    const youtubeLink = document.getElementById("youtube-link").value;
+    const videoContainer = document.getElementById("video-container");
+    const videoFrame = document.getElementById("youtube-video");
+    const playButton = document.getElementById("play-button");
+
+    if (isVideoPlaying) {
+        // Dừng video: Xóa src để dừng video hoàn toàn
+        videoFrame.src = "";
+        videoContainer.style.display = "none";
+        document.body.style.backgroundImage = "url('./1.gif')"; // Hiển thị lại GIF
+        playButton.textContent = "Play YouTube";
+        isVideoPlaying = false;
+    } else {
+        // Phát video với auto-play, âm thanh bật, và lặp lại video
+        const videoId = youtubeLink.split("v=")[1];
+        const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=0&loop=1&playlist=${videoId}`;
+
+        videoContainer.style.display = "block";
+        document.body.style.backgroundImage = "none"; // Ẩn GIF
+        videoFrame.src = embedUrl;
+        playButton.textContent = "Stop";
+        isVideoPlaying = true;
+    }
+});
